0d8e98f1f15374dc626f0d44d86ddaf676022710
sittora <sittora@gmail.com>
2021-08-11 14:03:56 -0400

    Completed assignment not final

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks (block) {
+    return Math.abs(42 - block);
+}
+
+function distanceFromHqInFeet (block) {
+    return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(location, endPoint) {
+    return Math.abs((location - endPoint) * 264);
+}
+
+function calculatesFarePrice (start, destination) {
+    const feetTravel = calculatesFarePrice (start, destination);
+if (feetTravel <= 400) {
+    return 0;
+} else if (feetTravel > 400 && feetTravel <= 2000) {
+    return (feetTravel - 400) * 0.02;
+} else if (feetTravel > 2000 && feetTravel <= 2500) {
+    return 25;
+} else {
+    return "cannot travel that far";
+}
+}
+
